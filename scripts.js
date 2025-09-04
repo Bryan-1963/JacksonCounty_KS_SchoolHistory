f322cae2188593a15b37f5c2d227551f2ae08974
Bryan-1963 <bryanlstauffer@gmail.com>
2025-09-03 17:24:15 -0700

    V149

--- scripts.js
+++ scripts.js
@@ -220,12 +220,12 @@
 	// loadDocPageNum
 	//==========================================================================================	
 	function loadDocPageNum(pgNum){
-		console.log("in loadDocPageNum, rcd pgNum=" + pgNum + ". photoFilePath=" + docPages[pgNum]['photoFilePath'].toString());
+		//console.log("in loadDocPageNum, rcd pgNum=" + pgNum + ". photoFilePath=" + docPages[pgNum]['photoFilePath'].toString());
 
 		let docImg = document.getElementById('docPageImg');
-		console.log("docImg.src=|" + docImg.src + "|");
+		//console.log("docImg.src=|" + docImg.src + "|");
 		docImg.src = webRootLocation + docPages[pgNum]['photoFilePath'].toString();
-		console.log("docImg.src changed to |" + docImg.src + "|");
+		//console.log("docImg.src changed to |" + docImg.src + "|");
 		
 		//update image caption
 		let figCapt = document.getElementById('figCaption');
