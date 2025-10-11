12500d2aeeeb20bad079681820dae2aa255c95bf
Bryan-1963 <bryanlstauffer@gmail.com>
2025-10-11 13:58:46 -0700

    V01.553

--- scripts.js
+++ scripts.js
@@ -471,6 +471,7 @@
 				}
 				else {
 					await loadDocPages(webStack[webStackIndex].docPagePath,webStack[webStackIndex].docTitle,false);
+					docPgNum = webStack[webStackIndex].docCurrPg*1; //have to set again here since loadDocPages put it at 0
 					console.log("loaded doc pages, now moving to page number="+docPgNum);
 					loadDocPageNum(docPgNum);
 				}
